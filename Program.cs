6fab2dfb345cd653eceeda16c96e46f1604734c2
днс <@LT-KKG25BBH>
2022-10-13 22:00:49 +1100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            int k = strArray[0].Length;
+            int m = 0;
+            for (int i = 1; i < strArray.Length; i++)
+            {
+                
+                if (strArray[i].Length > k)
+                {
+                    k = strArray[i].Length;
+                    m = i;
+                }
+            }
+            Console.WriteLine("Самое длинное слов состоит из " + k + " букв");
+            Console.WriteLine("Это слово - " + strArray[m]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            str = str.Replace(" ", "");
+            string p = "";
+            foreach (char c in str)
+            {
+                p = c + p;
+            }
+            if (str == p)
+            {
+                Console.WriteLine("Введенное предложение является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Введенное предложение не является палиндромом");
+            } 
+            Console.ReadKey();
+        }
+    }
+}
